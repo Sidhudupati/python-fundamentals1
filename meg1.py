1e373196820b60e46217eca39f43bb613fe17d00
sidhu <sdupati4@gmail.com>
2023-05-22 22:40:09 +0530

    college

---
+++ meg1.py
@@ -0,0 +1,28 @@
+print("Welcome to Aadya's Kitchen!")
+print("Here's your menu")
+print("Soups:\n1.Hot & Sour Veg Soup:'219/-'\n2.Chicken Noodles Soup:'229/-'")
+print("Starters:\n3.Veg Manchurian:'259/-'\n4.Paneer 65:'259/-'\n5.Chicken Chilli:'299/-'\n6.Apollo Fish:'329/-'")
+print("Curries:\n7.Paneer Butter Masala:'329/-'\n8.Dal Makhani:'329/-'\n9.Butter Chicken:'359/-'\n10.Kadai Chicken:'359/-'")
+print("Rotis:\n11.Plain Naan:'59/-'\n12.Butter Naan:'69/-'\n13.Garlic Naan:'79/-'\n14.Roti:'49/-")
+print("Main Course:\n15.Veg Pulao:'289/-'\n16.Paneer Pulao:'299/-'\n17.Mushroom Pulao:'299/-'\n18.Curd Rice:'259/-'\n19.Chicken Dum Biryani:'399/-'\n20.Chicken Pulao:'389/-'\n21.Avakaya Mutton Biryani:'419/-'\n22.Fish Pulao:'419/-'")
+print("Desserts:\n23.Apricot Delight:'329/-'\n24.Sitaphal Delight:'329/-'\n25.Mango Alphonso:'259/-'")
+print("Drinks:\n26.Mineral Water:'39/-'\n27.Coke:'59/-'")
+now_menu={'1':'219/-','2':'229/-',
+          '3':'259/-','4':'259/-',
+          '5':'299/-','6':'329/-',
+          '7':'329/-','8':'329/-',
+          '9':'359/-','10':'359/-',
+          '11':'59/-','12':'69/-',
+          '13':'79/-','14':'49/-',
+          '15':'289/-','16':'299/-',
+          '17':'299/-','18':'259/-',
+          '19':'399/-','20':'389/-',
+          '21':'419/-','22':'419/-',
+          '23':'329/-','24':'329/-',
+          '25':'259/-','26':'39/-',
+          '27':'59/-' ,
+          }
+
+
+
+
